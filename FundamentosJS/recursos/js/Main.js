a194f5d5f51609190f53c024f287324278ab05c0
kite tasx <kitetasxpete@gmail.com>
2024-11-07 17:40:05 -0500

    se crea el proyecto con los ejemplos de Fundamentos JS

---
+++ FundamentosJS/recursos/js/Main.js
@@ -0,0 +1,40 @@
+/*Metodo mas usado*/
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+document.getElementById("demo").style.fontSize = "35px"
+
+/*SalidaId Datos*/
+window.alert(5+2);
+console.log(6+2);
+
+/*Declaracion*/
+let a,b,c;
+a = 5;
+b = 6;
+c = a + b;
+C
+document.getElementById("demo").innerHTML= c;
+
+/*Variables*/
+let p,q,r;
+p = 5;
+q = 7;
+const m = 8;
+r = p + q + m;
+document.getElementById("demo1").innerHTML= r;
+
+/*Tipos de Datos*/
+let num = 20; //Numero entero
+let name = "Kite Tasx"; //Cadena
+let km = 14.5; // Decimal
+let isColombiano = true;// Booleano
+let obj = {firstname:"Ana", lastname:"Perez"}; //objeto
+
+let y = 17 + "POO";
+let unir = "17" + "POO";
+windowtion
+window.alert(unir)
+
+/*Definicion de funcion*/
+function myFunction(){
+    document.getElementById("demo2").innerHTML = "New paragraph";
+}
